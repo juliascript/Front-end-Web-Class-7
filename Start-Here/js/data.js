e346e98330cf06096129910d8a5a9f53af266df4
Julia <juliageist23@gmail.com>
2016-10-18 11:01:47 -0700

    challenge 1 complete

--- Start-Here/js/data.js
+++ Start-Here/js/data.js
@@ -5,5 +5,29 @@ var data = [
         desc: "Foo Bar",
         image: "images/image-1.jpg",
         bgColor: "#afa"
-    }
+    },
+    {
+        title: "Kitty",
+        desc: "Foo Bar",
+        image: "images/image-2.jpg",
+        bgColor: "#afa"
+    },
+    {
+        title: "Cat",
+        desc: "Foo Bar",
+        image: "images/image-3.jpg",
+        bgColor: "#afa"
+    },
+    {
+        title: "Sutro",
+        desc: "Foo Bar",
+        image: "images/image-4.jpg",
+        bgColor: "#afa"
+    },
+    {
+        title: "River",
+        desc: "Foo Bar",
+        image: "images/image-5.jpg",
+        bgColor: "#afa"
+    },
 ];
